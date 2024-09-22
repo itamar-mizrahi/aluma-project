ad12e74590d718422d506f22b91b4eba0fcf7394
nechamadina <157369835+nechamadina@users.noreply.github.com>
2024-09-22 22:47:49 +0300

    Update AddLogo.js

--- front/src/AddLogo.js
+++ front/src/AddLogo.js
@@ -5,7 +5,7 @@ function AddLogo() {
     <div>
       <header>
         <h1>this is an h1 tag</h1>
-        <img src={logo}  alt="logo" />
+        {/* <img src={logo}  alt="logo" /> */}
         <p>
           Edit and save to reload and show logo.
         </p>
